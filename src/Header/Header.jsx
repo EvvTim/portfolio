9d82e1480a77b9acc1a189159a126d5482f98c7c
Evgeniy Tsimashchuk <evgeniytim94@gmail.com>
2021-08-29 14:51:50 +0200

    feat: add hamburger-button and navigation to header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Navigation from "./Navigation/Navigation";
+import HamburgerButton from "./HamburgerButton/HamburgerButton";
+
+const classes = {
+    hamburger: 'hamburger',
+    hamburgerActive: 'hamburger hamburger--active',
+    navigation: 'navigation',
+    navigationActive: 'navigation navigation--active'
+}
 
 function Header() {
+    const [hamburger, setHamburger] = useState(true)
     return (
-        <header>Header</header>
+        <header>
+            <HamburgerButton
+                className={hamburger ? classes.hamburger : classes.hamburgerActive}
+                onClick={() => hamburger ? setHamburger(false) : setHamburger(true)}
+            />
+            <Navigation className={hamburger ? classes.navigation : classes.navigationActive}/>
+        </header>
     )
 }
 
