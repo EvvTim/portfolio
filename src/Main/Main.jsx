405b0f1ecd91139ab1cb47977016db523d96fee9
Evgeniy Tsimashchuk <evgeniytim94@gmail.com>
2021-08-31 17:57:42 +0200

    feat: add project-section and my-toolkit-section

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HelloSection from "./HelloSection/HelloSection";
 import AboutMeSection from "./AboutMeSection/AboutMeSection";
+import ProjectsSection from "./ProjectsSection/ProjectsSection";
+import MyToolkit from "./MyToolkit/MyToolkit";
 
 function Main () {
     return (
         <main>
             <HelloSection/>
             <AboutMeSection/>
+            <ProjectsSection/>
+            <MyToolkit/>
         </main>
     )
 }
