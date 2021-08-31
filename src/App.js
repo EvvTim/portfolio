8c3e7e154dadae5151dd3390ab3481970ce295f9
Evgeniy Tsimashchuk <evgeniytim94@gmail.com>
2021-08-31 23:35:17 +0200

    feat: add nav animation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import './App.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
+import React, {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
+    const [hamburger, setHamburger] = useState(true)
+    const classes = {
+        hamburger: 'hamburger',
+        hamburgerActive: 'hamburger hamburger--active',
+        navigation: 'navigation',
+        navigationActive: 'navigation navigation--active',
+        app: 'App',
+        appActive: 'App App--active'
+    }
+    console.log(hamburger)
+    return (
+    <div className={hamburger ? classes.app : classes.appActive}>
+        <Header
+            onClick={() => hamburger ? setHamburger(false) : setHamburger(true)}
+            hamburgerActive={hamburger ? classes.hamburger : classes.hamburgerActive}
+            navigationActive={hamburger ? classes.navigation : classes.navigationActive}
+        />
         <Main/>
         <Footer/>
     </div>
