ec252963e7086268c1776e6d5788bc682f939422
Evgeniy Tsimashchuk <evgeniytim94@gmail.com>
2021-08-31 17:58:20 +0200

    feat: add toolkit link

--- src/Header/Navigation/Navigation.jsx
+++ src/Header/Navigation/Navigation.jsx
@@ -6,7 +6,7 @@ function Navigation ({className}) {
             <ul>
                 <li><a href="#main">Main<span className='nav-border'></span></a></li>
                 <li><a href="#about">About<span className='nav-border'></span></a></li>
-                <li><a href="#skills">Skills<span className='nav-border'></span></a></li>
+                <li><a href="#toolkit">My Toolkit<span className='nav-border'></span></a></li>
                 <li><a href="#projects">Projects<span className='nav-border'></span></a></li>
                 <li><a href="#contacts">Contacts<span className='nav-border'></span></a></li>
             </ul>
